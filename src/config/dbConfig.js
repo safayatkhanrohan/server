5189b7d9e1f86f321ebf79bc7684b8d72a3a1426
Safayat Khan Rohan <safayatkhanrohan@gmail.com>
2024-08-20 20:30:49 +0600

    add vercel.json

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/estore');
-        console.log(`MongoDB connected`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
 
 module.exports = connectDB;
--- src/helper/jwt.js
+++ src/helper/jwt.js
@@ -1,11 +1,14 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: 60*60*24*7}); // 7 days
-}
+  console.log(process.env.JWT_SECRET);
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: 60 * 60 * 24 * 7,
+  }); // 7 days
+};
 
 const varifyToken = (token) => {
-    return jwt.verify(token, process.env.JWT_SECRET);
-}
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
 
-module.exports = {createToken, varifyToken};
+module.exports = { createToken, varifyToken };
